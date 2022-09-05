dd7ab42d10bbb79a7d6935a36607de5152d6319d
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-05 14:43:40 -0700

    import uploadForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import UploadForm from "./UploadForm";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <UploadForm />
     </div>
   );
 }
