5690642b91e68884e4fe3bea3eb51643792b4130
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-05 16:13:07 -0700

    create function to handle file uploads

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,28 @@
+import { useState, useEffect} from "react";
+import { imgStorage } from './firebase';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+
+const useStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    const storageRef = ref(imgStorage, file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',(snapshot) => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setProgress(percentage);
+    }, (err) => {
+      setError(err);
+    }, () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((url) =>
+      setUrl(url));
+    });
+  }, [file]);
+
+  return  { progress, url, error }
+}
+
+export default useStorage;
