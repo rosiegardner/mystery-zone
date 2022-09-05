92f23997df2abb4bec043edbe7a5bf51d45cf9a0
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-05 13:58:13 -0700

    create file with component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Drag the Queen</h1>
+      <h2>Welcome to Doom Town PDX</h2>
+      <p>The decline of Portland Punk Civilization</p>
+    </div>
+  )
+}
+
+export default Header;
