f0b607579bb0646471c28a6e0c015e3229d9aa6b
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-06 21:36:50 -0700

    WIP: why is a timestamp so confusing

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -2,6 +2,7 @@ import { useState, useEffect} from "react";
 import { imgStorage, db } from '../firebase';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
+
 const useStorage = (file) => {
   const [progress, setProgress] = useState(0);
   const [error, setError] = useState(null);
@@ -9,7 +10,8 @@ const useStorage = (file) => {
 
   useEffect(() => {
     const storageRef = ref(imgStorage, file.name);
-
+    // const collectionRef = db.collection('images');
+  
     const uploadTask = uploadBytesResumable(storageRef, file);
 
     uploadTask.on('state_changed',(snapshot) => {
@@ -27,3 +29,36 @@ const useStorage = (file) => {
 }
 
 export default useStorage;
+
+
+// import { useState, useEffect } from 'react';
+// import { projectStorage, projectFirestore, timestamp } from '../firebase/config';
+
+// const useStorage = (file) => {
+//   const [progress, setProgress] = useState(0);
+//   const [error, setError] = useState(null);
+//   const [url, setUrl] = useState(null);
+
+//   useEffect(() => {
+//     // references
+//     const storageRef = projectStorage.ref(file.name);
+//     const collectionRef = projectFirestore.collection('images');
+
+//     storageRef.put(file).on('state_changed', (snap) => {
+//       let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+//       setProgress(percentage);
+//     }, (error) => {
+//       setError(error);
+//     }, async () => {
+//       const url = await storageRef.getDownloadURL();
+//       const createdAt = timestamp();
+//       collectionRef.add({ url, createdAt });
+//       setUrl(url);
+//     })
+//   }, [file]);
+
+//   return { progress, url, error }
+
+// }
+
+// export default useStorage;
