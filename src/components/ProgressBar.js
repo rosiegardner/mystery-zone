e1f0afadfc6b797b83b1b120b001c85ad87371c2
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-07 20:03:54 -0700

    WIP

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
 
 const ProgressBar = ({ file, setFile }) => {
-  const {url, progress } = useStorage(file);
+  const {progress, url} = useStorage(file);
 
 
   useEffect(() => {
