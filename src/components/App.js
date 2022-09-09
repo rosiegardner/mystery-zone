e2e1252482e158c97ba226e970c8d23b6a6683a2
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-09 10:29:13 -0700

    import ImageGrid component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import UploadForm from "./UploadForm";
+import ImageGrid from "./ImageGrid";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <UploadForm />
+      <ImageGrid />
     </div>
   );
 }
