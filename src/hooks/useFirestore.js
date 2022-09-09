1e5fceecb807c54a46c9fa23e7db4df29cba4550
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-09 11:11:00 -0700

    WIP: add useEffect - rewrite code

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,10 +1,48 @@
 import { useState, useEffect } from 'react';
 import { db } from '../firebase.js';
+// import { collection, addDoc } from 'firebase/firestore';
+// import { collection, addDoc, doc, updateDoc, onSnapshot, deleteDoc, query, orderBy } from "firebase/firestore";
 
 const useFirestore = (collection) => {
   const [docs, setDocs] = useState([]);
+
+  useEffect (() => {
+    const unSubscribe = db.collection(collection)
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((snap) => {
+        let documents = [];
+        snap.forEach(doc => {
+          documents.push({...doc.data(), id: doc.id})
+        });
+        setDocs(documents);
+      });
+
+    return () => unSubscribe();
+
+  }, [collection])
 
   return { docs };
 }
 
 export default useFirestore;
+
+
+  // ** Possible Updated code for R.V18 ** 
+  
+    // const queryByTimestamp = query(
+    //   collection(db, "images"),
+    //   orderBy('createdAt', 'desc')
+    // );
+
+    // const unSubscribe = onSnapshot(
+    //   queryByTimestamp,
+    //   (querySnapshot) => {
+    //     let documents = [];
+    //     querySnapshot.forEach((doc) => {
+    //       documents.push({...doc.data(), id: doc.id})
+    //     });
+    //     setDocs(documents);
+    //   });
+
+    //   return () => unSubscribe();
+    // )
