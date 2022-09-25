949a8ff1e3c507032b5e09805c9fc885c26c0d1e
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-25 16:21:12 -0700

    add delete handler

--- src/components/albums/AlbumDetail.js
+++ src/components/albums/AlbumDetail.js
@@ -2,19 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function AlbumDetail(props){
-  const { album } = props;
+  const { album, albumDelete } = props;
 
   return (
     <React.Fragment>
       <h1>Album Detail</h1>
       <h3>{album.title}</h3>
+      <button onClick = {() => albumDelete(album.id) }>Delete Album</button>
       <hr/>
     </React.Fragment>
   );
 }
 
 AlbumDetail.propTypes = {
-  album: PropTypes.object
+  album: PropTypes.object,
+  albumDelete: PropTypes.func
 };
 
 export default AlbumDetail;
