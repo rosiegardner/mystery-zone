88fb8166b9a6b326bfe45cd301354aa350c2f35f
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-25 15:00:53 -0700

    move main album list into state

--- src/components/albums/AlbumControl.js
+++ src/components/albums/AlbumControl.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import NewAlbumForm from './NewAlbumForm';
-import Albumlist from './AlbumList';
+import AlbumList from './AlbumList';
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 
 class AlbumControl extends React.Component {
 
-  contructor(props) {
+  constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false,
+      mainAlbumList: []
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    
+    if(this.state.formVisibleOnPage) {
+      currentlyVisibleState = 
+        <NewAlbumForm />;
+        buttonText = "Return to Album List"
+    } else {
+      currentlyVisibleState = 
+        <AlbumList 
+          albumList={this.state.mainAlbumList} />
+        buttonText = "Add Album"
+    }
+
     return (
       <React.Fragment>
-
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
