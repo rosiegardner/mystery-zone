22ef213f157c6195c8cf8e70ec7ec62d9a287ada
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-25 12:24:36 -0700

    import AlbumList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Header from "./Header";
+import AlbumList from './albums/AlbumList';
 import UploadForm from "./UploadForm";
 import ImageGrid from "./ImageGrid";
 import Modal from "./Modal";
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <AlbumList />
       <UploadForm />
       <ImageGrid setSelectedImage={setSelectedImage} />
       { selectedImage && <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage} /> }
