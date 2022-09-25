fcf1f4d4dc74e5dd796374e89d3718d5978aaacc
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-25 16:12:20 -0700

    add props

--- src/components/albums/AlbumDetail.js
+++ src/components/albums/AlbumDetail.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function AlbumDetail(props){
+  const { album } = props;
+
   return (
     <React.Fragment>
       <h1>Album Detail</h1>
+      <h3>{album.title}</h3>
       <hr/>
     </React.Fragment>
   );
 }
+
+AlbumDetail.propTypes = {
+  album: PropTypes.object
+};
 
 export default AlbumDetail;
