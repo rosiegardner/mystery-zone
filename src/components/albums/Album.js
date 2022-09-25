1a6ba4154e1d2960c61c93fbfb77bcbac6b66c03
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-25 12:55:54 -0700

    import proptypes

--- src/components/albums/Album.js
+++ src/components/albums/Album.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 
-function Album() {
+function Album(props) {
   return (
     <React.Fragment>
       <h1>Album</h1>
+      <h3>{props.title}</h3>
       <hr/>
     </React.Fragment>
   );
 }
+
+Album.propTypes = {
+  title: Proptypes.string.isRequired
+};
 
 export default Album;
