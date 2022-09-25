13d170f6c04d663ad35f90099dc53d68dc5fd4d7
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-25 16:28:31 -0700

    add component for form reuse

---
+++ src/components/albums/ReusableForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={formSubmissionHandler}>
+        <input 
+          type='text'
+          title='title'
+          placeholder='"Album 1"' />
+        <button type='submit'>{props.buttonText}</button>
+        </form>
+    </React.Fragment>
+  );
+
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
