d760c06a1b4c475b183253dd9eef1fc168ff3eb8
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-25 14:07:29 -0700

    create component for new album

---
+++ src/components/albums/NewAlbumForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NewAlbumForm(props){
+  return (
+    <React.Fragment>
+      <h3>Album Name</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewAlbumForm;
