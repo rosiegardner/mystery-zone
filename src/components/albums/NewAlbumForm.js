4425c4bb7f1eab2076601a7eaeb3f142a4930277
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-25 15:00:08 -0700

    add form and handle click

--- src/components/albums/NewAlbumForm.js
+++ src/components/albums/NewAlbumForm.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 function NewAlbumForm(props){
+
+  function handleNewAlbumForm(event) {
+    event.preventDefault();
+    console.log(event.target.type.value);
+    console.log(event.target.name.value);
+    console.log(event.target.issue.value);
+  };
+  
   return (
     <React.Fragment>
-      <h3>Album Name</h3>
+      <form onSubmit={handleNewAlbumForm}>
+        <input 
+          type='text'
+          name='name'
+          placeholder='"Album 1"' />
+        <button type='submit'>Create Album!</button>
+        </form>
     </React.Fragment>
   );
 }
