0f17f9fdf273747b4528961d9b50b793a0021dba
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-25 14:11:42 -0700

    add skeleton for album control comp

---
+++ src/components/albums/AlbumControl.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NewAlbumForm from './NewAlbumForm';
+import Albumlist from './AlbumList';
+
+class AlbumControl extends React.Component {
+
+  contructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default AlbumControl;
