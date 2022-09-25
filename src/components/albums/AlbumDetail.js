0e854942365d215098dbfa15fb4829977575ff56
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-25 15:48:50 -0700

    create placeholder component

---
+++ src/components/albums/AlbumDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AlbumDetail(props){
+  return (
+    <React.Fragment>
+      <h1>Album Detail</h1>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default AlbumDetail;
