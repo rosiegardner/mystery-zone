6bdd6b0a66bfede7567e41e458c1f5e5b106b65d
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-26 20:01:16 -0700

    refactor code for firebase

--- src/components/albums/AlbumControl.js
+++ src/components/albums/AlbumControl.js
@@ -1,14 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NewAlbumForm from './NewAlbumForm';
 import AlbumList from './AlbumList';
 import AlbumDetail from './AlbumDetail';
 import EditAlbumForm from './EditAlbumForm';
+import { db } from '../.././firebase.js';
+import { collection, addDoc, doc, onSnapshot, updateDoc, deleteDoc } from 'firebase/firestore';
 
 function AlbumControl() {
   const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
   const [mainAlbumList, setMainAlbumList] = useState([]);
   const [selectedAlbum, setSelectedAlbum] = useState(null);
   const [editing, setEditing] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const unSubscribe = onSnapshot(
+      collection(db, "albums"),
+      (collectionSnapshot) => {
+        const albums = [];
+        collectionSnapshot.forEach((doc) => {
+          albums.push({
+            ...doc.data(), id: doc.id
+          });
+        });
+        setMainAlbumList(albums);
+      }, 
+      (error) => {
+        setError(error.message);
+      }
+    );
+    return () => unSubscribe();
+  }, []);
+
 
   const handleClick = () => {
     if (selectedAlbum != null) {
@@ -24,11 +47,10 @@ function AlbumControl() {
     setEditing(true);
   }
 
-  const handleAddingNewAlbumToList = (newAlbum) => {
-    const newMainAlbumList = mainAlbumList
-      .concat(newAlbum);
-    setMainAlbumList(newMainAlbumList);
-    setFormVisibleOnPage(false); 
+  const handleAddingNewAlbumToList = async (newAlbumData) => {
+    await addDoc(collection(db, "albums"), newAlbumData);
+    setFormVisibleOnPage(false);
+   
   }
 
   const handleChangingSelectedAlbum = (id) => {
@@ -37,18 +59,13 @@ function AlbumControl() {
     setSelectedAlbum(selection);
   }
 
-  const handleDeletingAlbum = (id) => {
-    const newMainAlbumList = mainAlbumList
-      .filter(album => album.id !== id);
-    setMainAlbumList(newMainAlbumList);
+  const handleDeletingAlbum = async (id) => {
+    await deleteDoc(doc(db, "tickets", id));
     setSelectedAlbum(null);
   }
 
-  const handleEditingAlbum = (albumToEdit) => {
-    const editedMainAlbumList = mainAlbumList
-      .filter(album => album.id !== selectedAlbum.id)
-      .concat(albumToEdit);
-    setMainAlbumList(editedMainAlbumList);
+  const handleEditingAlbum =  async (albumToEdit) => {
+    await updateDoc(doc(db, "albums", albumToEdit.id), albumToEdit);
     setEditing(false);
     setSelectedAlbum(null);
   }
@@ -56,7 +73,9 @@ function AlbumControl() {
     let currentlyVisibleState = null;
     let buttonText = null;
     
-    if (editing) {
+    if (error) {
+      currentlyVisibleState = <p>There was an error: {error}</p>
+    } else if (editing) {
       currentlyVisibleState = 
       <EditAlbumForm 
         album = {selectedAlbum}
@@ -85,7 +104,7 @@ function AlbumControl() {
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        <button onClick={handleClick}>{buttonText}</button>
+        {error ? null : <button onClick={handleClick}>{buttonText}</button>}
       </React.Fragment>
     );
   
