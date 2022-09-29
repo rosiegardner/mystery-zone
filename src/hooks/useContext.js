fc4cd1d597e1a7a00b66ee553df25bddb642f2e1
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-28 19:22:34 -0700

    set file to use context

---
+++ src/hooks/useContext.js
@@ -0,0 +1,27 @@
+import { useContext, createContext, useState, useMemo } from 'react'
+
+const AlbumContext = createContext()
+
+const AlbumProvider = ({ children }) => {
+  const [album, setAlbum] = useState({})
+
+  const value = useMemo(() => ({ album, setAlbum }), [album])
+  return <AlbumContext.Provider value={value}>{children}</AlbumContext.Provider>
+
+}
+
+const useAlbum = () => {
+  const context = useContext(AlbumContext)
+  if (context === undefined) {
+    throw new Error('useAlbum needs to be defined within the AlbumContext Provider')
+  }
+  return context
+}
+
+export { AlbumContext, AlbumProvider, useAlbum }
+
+// Inside any component that is wrapped in your <AlbumProvider>:
+// import useAlbum from './hooks/AlbumContext' << or something
+// const [album, setAlbum] = useAlbum()
+
+// setAlbum(newAlbumFromTheForm)
