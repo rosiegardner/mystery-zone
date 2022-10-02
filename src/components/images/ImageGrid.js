4b30205537cc230669575425ad474d7e8d319833
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-02 14:22:20 -0700

    move img components into images folder

---
+++ src/components/images/ImageGrid.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import useFirestore from '../.././hooks/useFirestore';
+import PropTypes from 'prop-types';
+
+function ImageGrid({ setSelectedImage, albumId }){
+  const { docs } = useFirestore(albumId);
+
+  return (
+    <div className="img-grid">
+      { docs && docs.map(doc => (
+        <div className="img-wrap" key={doc.id}
+        onClick={() => setSelectedImage(doc.url)}>
+          <img src={doc.url} alt="uploaded" />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+ImageGrid.propTypes ={
+  albumId: PropTypes.string
+};
+
+export default ImageGrid;
---
+++ src/components/images/ProgressBar.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import useStorage from '../.././hooks/useStorage';
+
+const ProgressBar = ({ file, setFile, albumId }) => {
+  const {progress, url} = useStorage(file, albumId);
+
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile, albumId]);
+
+  return (
+    <div className="progress-bar" style={{ width: progress + '%' }}></div>
+  )
+}
+
+export default ProgressBar;
