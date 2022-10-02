ad156366bf5bc657fd39b64cfd46b34fcc145df2
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-02 11:07:25 -0700

    remove: img components from app.js, import: img components to albumdetail.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
-
+import React from "react";
 import Header from "./Header";
 import AlbumControl from './albums/AlbumControl';
-import UploadForm from "./UploadForm";
-import ImageGrid from "./ImageGrid";
-import Modal from "./Modal";
+
 
 function App() {
-  const [selectedImage, setSelectedImage] = useState(null);
   
   return (
     <div className="App">
       <Header />
       <AlbumControl />
-      <UploadForm />
-      <ImageGrid setSelectedImage={setSelectedImage} />
-      { selectedImage && <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage} /> }
     </div>
   );
 }
--- src/components/albums/AlbumDetail.js
+++ src/components/albums/AlbumDetail.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import UploadForm from '../UploadForm';
+import ImageGrid from '../ImageGrid';
+import Modal from '../Modal';
 
 function AlbumDetail(props){
   const { album, albumDelete } = props;
+  const [selectedImage, setSelectedImage] = useState(null);
 
   return (
     <React.Fragment>
       <h1>Album Detail</h1>
       <h3>{album.name}</h3>
+      <UploadForm />
+      <ImageGrid setSelectedImage={setSelectedImage} />
+      { selectedImage && <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage} /> }
       <button onClick = { props.albumEdit }>Update Album</button>
       <button onClick = {() => albumDelete(album.id) }>Delete Album</button>
       <hr/>
