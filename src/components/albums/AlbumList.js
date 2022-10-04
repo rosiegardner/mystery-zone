1c6dd3ed7b2b8bc6ce5fc576f1d7569a8c354862
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-03 19:55:04 -0700

    add styling to album list

--- src/components/albums/AlbumList.js
+++ src/components/albums/AlbumList.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Album from './Album';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
 
 function AlbumList(props){
   return (
     <React.Fragment>
+      <div className="album-grid">
+        {props.albumList.map((album) => 
+        <motion.div className="album-wrap"
+          layout
+          whileHover={{ opacity: 1 }}>
+          <Album 
+            albumClicked = { props.albumSelected }
+            name = {album.name}
+            id = {album.id}
+            key = {album.id} />
+        </motion.div>
+        )}
+
+      </div>
       <hr />
-      {props.albumList.map((album) => 
-      <Album 
-        albumClicked = { props.albumSelected }
-        name = {album.name}
-        id = {album.id}
-        key = {album.id} />
-      )}
     </React.Fragment>
   );
 }
