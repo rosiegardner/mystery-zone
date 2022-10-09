144f5634dc37fe479697e644533564df3d60bc7e
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-08 23:17:35 -0700

    prep for merge

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import Header from "./Header";
 import Home from './Home';
 import AlbumControl from './albums/AlbumControl';
-// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-// import { AnimatePresence } from 'framer-motion';
-
-
 
 function App() {
   
@@ -14,19 +10,6 @@ function App() {
       <Header/>
       <Home/>
       <AlbumControl/>
-      {/* <Router>
-        <Header />
-        <AnimatePresence wait>
-          <Routes>
-            <Route path="/" element={<AlbumControl />}/>
-            <Route path="/home" element={<Home />}/>
-          </Routes>
-        </AnimatePresence>
-
-      </Router> */}
-      {/* <Home/>
-      <AlbumControl /> */}
-      
     </div>
   );
 }
--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -30,25 +30,3 @@ const useFirestore = (albumId) => {
 }
 
 export default useFirestore;
-
-  // ** Old CODE **
-    // const useFirestore = (collection) => {
-    //   const [docs, setDocs] = useState([]);
-    
-    //   useEffect (() => {
-    //     const unSubscribe = db.collection(collection)
-    //       .orderBy('createdAt', 'desc')
-    //       .onSnapshot((snap) => {
-    //         let documents = [];
-    //         snap.forEach(doc => {
-    //           documents.push({...doc.data(), id: doc.id})
-    //         });
-    //         setDocs(documents);
-    //       });
-    
-    //     return () => unSubscribe();
-    
-    //   }, [collection])
-    
-    //   return { docs };
-    // }
